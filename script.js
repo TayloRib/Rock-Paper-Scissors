842920b7013fa9a1684e4034924f376f5046a63b
Ribbit <taylor.orsini@yahoo.com>
2023-03-29 10:59:06 -0700

    status criteria

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert("Welcome to our rock, paper, scissors game!");
+
+let userChoice = prompt("Please select R, P, or S to start playing.") 
+
+console.log(userChoice)
+
+
+function winMessage() {
+    console.log("Congrats! You won.")
+}
+
+function loseMessage() {
+    console.log("Sorry! You lost.")
+}
+
+function tieMessage() {
+    console.log("Darn! You tied.")
+}
